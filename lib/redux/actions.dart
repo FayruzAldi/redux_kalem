d78cbb80fb4295af8ff4049aa908afd7ec9f1683
Aldi <fayruzaldi0@gmail.com>
2024-09-04 16:05:29 +0700

    Feat:[AF] Menambahkan Fitur Kalkulator-BMI

---
+++ lib/redux/actions.dart
@@ -0,0 +1,22 @@
+class CalculateBMI {
+  final double height;
+  final double weight;
+
+  CalculateBMI(this.height, this.weight);
+}
+
+class CalculateArithmetic {
+  final double number1;
+  final double number2;
+  final String operation;
+
+  CalculateArithmetic(this.number1, this.number2, this.operation);
+}
+
+class CalculateShapeArea {
+  final double dimension1;
+  final double dimension2;
+  final String shape;
+
+  CalculateShapeArea(this.dimension1, this.dimension2, this.shape);
+}
