07745a00b53efd748b7d55b5abf196a80de55ced
azzam1307 <azzamnh131007@gmail.com>
2024-09-04 17:58:59 +0700

    Feat:[AFS] Menambahkan Fitur Kalkulator-Bangundatar

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return StoreProvider<AppState>(
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomePage(),
+        home: const HomePage(),
       ),
     );
   }
