82cf5f817b86b17721899390497bef68f22a3f8f
Aldi <fayruzaldi0@gmail.com>
2024-09-04 17:39:08 +0700

    Tambahkan perubahan pada bmi_screen.dart dan file gradle

--- lib/screen/bmi_screen.dart
+++ lib/screen/bmi_screen.dart
@@ -51,14 +51,3 @@ class BMIScreen extends StatelessWidget {
     );
   }
 }
-
-import 'package:flutter/material.dart';
-
-class BmiScreen extends StatelessWidget {
-  const BmiScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
